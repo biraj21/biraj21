a1d67b0d957e6d4d2b5cca39396a72472d0fd4fd
Biraj <biraj795@gmail.com>
2022-03-25 09:08:58 +0530

    added dynamic transition delays

--- js/main.js
+++ js/main.js
@@ -5,17 +5,33 @@ let active_page = 0;
 
 window.addEventListener("load", () => {
     pages = document.querySelectorAll(".page");
+    nav_btns = document.querySelectorAll(".nav-btn");
 
     document.querySelectorAll(".next-page-btn")
         .forEach(btn => {
             btn.addEventListener("click", () => load_page(active_page + 1));
         });
 
-    nav_btns = document.querySelectorAll(".nav-btn");
     nav_btns.forEach((btn, i) => {
         btn.addEventListener("click", () => load_page(i));
     });
 
+    // adding transition delays
+    let skills = document.querySelectorAll(".skills .skill");
+    for (let i = 1; i < skills.length; ++i)
+        skills[i].style.animationDelay = `${50 * (i + 1)}ms`;
+
+    document.querySelectorAll(".tools .skill")
+        .forEach((el, i) => {
+            el.style.animationDelay = `${50 * (skills.length + i + 1)}ms`;
+        });
+
+    document.querySelectorAll(".project:not(:first-child)")
+        .forEach((el, i) => {
+            el.style.animationDelay = `${100 + 50 * i}ms`;
+        });
+
+    // hiding other pages
     for (let i = active_page + 1; i < pages.length; ++i)
         pages[i].classList.add("page--inactive");
 });
