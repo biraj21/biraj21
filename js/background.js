2ec1f089e0fb3df0413372a4aa2923cf06d65344
Biraj <biraj.pub@gmail.com>
2022-10-17 14:29:04 +0530

    refactoring & ui improvements

--- js/background.js
+++ js/background.js
@@ -1,32 +1,34 @@
 "use strict";
 
 class Particle {
-    constructor(radius, x, y, dx, dy, color) {
-        this.radius = radius;
-        this.x = x;
-        this.y = y;
-        this.dx = dx;
-        this.dy = dy;
-        this.color = color;
+  constructor(radius, x, y, dx, dy, color) {
+    this.radius = radius;
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.color = color;
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+  update() {
+    if (this.x < this.radius || this.x > canvas.width - this.radius) {
+      this.dx *= -1;
     }
 
-    draw(ctx) {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-        ctx.fillStyle = this.color;
-        ctx.fill();
+    if (this.y < this.radius || this.y > canvas.height - this.radius) {
+      this.dy *= -1;
     }
 
-    update() {
-        if (this.x < this.radius || this.x > canvas.width - this.radius)
-            this.dx *= -1;
-
-        if (this.y < this.radius || this.y > canvas.height - this.radius)
-            this.dy *= -1;
-
-        this.x += this.dx;
-        this.y += this.dy;
-    }
+    this.x += this.dx;
+    this.y += this.dy;
+  }
 }
 
 const canvas = document.getElementById("background");
@@ -41,61 +43,57 @@ let reqId = null;
 window.addEventListener("load", init);
 
 window.addEventListener("resize", () => {
-    canvas.width = window.innerWidth * 2;
-    canvas.height = window.innerHeight * 2;
+  canvas.width = window.innerWidth * 2;
+  canvas.height = window.innerHeight * 2;
 
-    init();
+  init();
 });
 
 function init() {
-    let n_particles = Math.min(
-        500,
-        Math.round((canvas.width * canvas.height) / 9500)
-    );
+  let numParticles = Math.min(500, Math.round((canvas.width * canvas.height) / 9500));
 
-    particles = [];
+  particles = [];
 
-    for (let i = 0; i < n_particles; ++i) {
-        let r = Math.random() * 3 + 1;
-        let x = Math.random() * (canvas.width - r) + r;
-        let y = Math.random() * (canvas.height - r) + r;
-        let dx = (Math.random() > 0.5 ? 1 : -1) * Math.random() * 2;
-        let dy = (Math.random() > 0.5 ? 1 : -1) * Math.random() * 2;
+  for (let i = 0; i < numParticles; ++i) {
+    let r = Math.random() * 3 + 1;
+    let x = Math.random() * (canvas.width - r) + r;
+    let y = Math.random() * (canvas.height - r) + r;
+    let dx = (Math.random() > 0.5 ? 1 : -1) * Math.random() * 1.5;
+    let dy = (Math.random() > 0.5 ? 1 : -1) * Math.random() * 1.5;
 
-        particles.push(new Particle(r, x, y, dx, dy, "#fff"));
-    }
+    particles.push(new Particle(r, x, y, dx, dy, "#fff"));
+  }
 
-    if (reqId != null)
-        cancelAnimationFrame(reqId);
+  if (reqId != null) cancelAnimationFrame(reqId);
 
-    animate();
+  animate();
 }
 
 function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (let p of particles) {
-        p.draw(ctx);
-        p.update();
-    }
+  for (let p of particles) {
+    p.draw(ctx);
+    p.update();
+  }
 
-    connect();
-    reqId = requestAnimationFrame(animate);
+  connect();
+  reqId = requestAnimationFrame(animate);
 }
 
 function connect() {
-    for (let p1 of particles) {
-        for (let p2 of particles) {
-            let distance = Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+  for (let p1 of particles) {
+    for (let p2 of particles) {
+      let distance = Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
 
-            if (distance < 100) {
-                ctx.beginPath();
-                ctx.strokeStyle = `#f00`;
-                ctx.lineWidth = 1;
-                ctx.moveTo(p1.x, p1.y);
-                ctx.lineTo(p2.x, p2.y);
-                ctx.stroke();
-            }
-        }
+      if (distance < 100) {
+        ctx.beginPath();
+        ctx.strokeStyle = "#808080";
+        ctx.lineWidth = 1;
+        ctx.moveTo(p1.x, p1.y);
+        ctx.lineTo(p2.x, p2.y);
+        ctx.stroke();
+      }
     }
+  }
 }
--- js/main.js
+++ js/main.js
@@ -1,52 +1,51 @@
 "use strict";
 
 let pages;
-let nav_btns;
+let navBtns;
 
-let active_page = 0;
+let activePage = 0;
 
 window.addEventListener("load", () => {
-    pages = document.querySelectorAll(".page");
-    nav_btns = document.querySelectorAll(".nav-btn");
+  pages = document.querySelectorAll(".page");
+  navBtns = document.querySelectorAll(".nav-btn");
 
-    document.querySelectorAll(".next-page-btn")
-        .forEach(btn => {
-            btn.addEventListener("click", () => load_page(active_page + 1));
-        });
+  document.querySelectorAll(".next-page-btn").forEach((btn) => {
+    btn.addEventListener("click", () => loadPage(activePage + 1));
+  });
 
-    nav_btns.forEach((btn, i) => {
-        btn.addEventListener("click", () => load_page(i));
-    });
+  navBtns.forEach((btn, i) => {
+    btn.addEventListener("click", () => loadPage(i));
+  });
 
-    // adding transition delays
-    let skills = document.querySelectorAll(".skills .skill");
-    for (let i = 1; i < skills.length; ++i)
-        skills[i].style.animationDelay = `${50 * (i + 1)}ms`;
+  // adding transition delays
+  let skills = document.querySelectorAll(".skills .skill");
+  for (let i = 1; i < skills.length; ++i) {
+    skills[i].style.animationDelay = `${50 * (i + 1)}ms`;
+  }
 
-    document.querySelectorAll(".tools .skill")
-        .forEach((el, i) => {
-            el.style.animationDelay = `${50 * (skills.length + i + 1)}ms`;
-        });
+  document.querySelectorAll(".tools .skill").forEach((el, i) => {
+    el.style.animationDelay = `${50 * (skills.length + i + 1)}ms`;
+  });
 
-    document.querySelectorAll(".project:not(:first-child)")
-        .forEach((el, i) => {
-            el.style.animationDelay = `${100 + 50 * i}ms`;
-        });
+  document.querySelectorAll(".project:not(:first-child)").forEach((el, i) => {
+    el.style.animationDelay = `${100 + 50 * i}ms`;
+  });
 
-    // hiding other pages
-    for (let i = active_page + 1; i < pages.length; ++i)
-        pages[i].classList.add("page--inactive");
+  // hiding other pages
+  for (let i = activePage + 1; i < pages.length; ++i) {
+    pages[i].classList.add("page--inactive");
+  }
 });
 
-function load_page(page_no) {
-    nav_btns[active_page].classList.remove("nav-btn--active");
+function loadPage(pageNo) {
+  navBtns[activePage].classList.remove("nav-btn--active");
 
-    // making current page inactive
-    pages[active_page].classList.add("page--inactive");
-    pages[active_page].classList.remove("page--loaded");
+  // making current page inactive
+  pages[activePage].classList.add("page--inactive");
+  pages[activePage].classList.remove("page--active");
 
-    active_page = page_no;
-    nav_btns[active_page].classList.add("nav-btn--active");
-    pages[active_page].classList.remove("page--inactive");
-    pages[active_page].classList.add("page--loaded");
+  activePage = pageNo;
+  navBtns[activePage].classList.add("nav-btn--active");
+  pages[activePage].classList.remove("page--inactive");
+  pages[activePage].classList.add("page--active");
 }
