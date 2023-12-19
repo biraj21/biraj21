d9b8b4f82ace45042cf8aa23fc08a194585282bf
Biraj <biraj.pub@gmail.com>
2023-12-19 20:06:10 +0530

    style(home): increase font size

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -3,14 +3,14 @@ import NextSectionLink from "@/components/NextSectionLink";
 export default function Home() {
   return (
     <section className="section items-start">
-      <h1 className="text-primary text-3xl font-bold">
+      <h1 className="text-primary text-5xl font-bold">
         Hello, I'm Biraj! <br />
-        <span className="text-2xl">
+        <span className="text-4xl leading-none">
           I turn code into <em>impactful products</em>.
         </span>
       </h1>
 
-      <p className="max-w-sm my-4 text-justify">
+      <p className="max-w-sm my-4 text-justify text-xl">
         I'm a Software Developer based in Bangalore, India. Seamlessly integrating user-friendly experiences with robust
         code, I fuse technology and business insight to deliver comprehensive solutions.
       </p>
