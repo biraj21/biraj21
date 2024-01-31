7e8a02554b090058ba49cdf29efaea58162d0912
Biraj <biraj.pub@gmail.com>
2024-01-31 23:10:16 +0530

    fix(Nav): use button to replace to avoid creating history

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,13 +11,13 @@ export default function Nav() {
   return (
     <nav className="fixed right-4 top-1/2 -translate-y-1/2 flex flex-col p-1 border-2 border-gray-800 rounded-full backdrop-blur-md drop-shadow-lg">
       {SECTIONS.map((section) => (
-        <a
+        <button
           key={section.id}
-          href={`#${section.id}`}
+          onClick={() => window.location.replace("#" + section.id)}
           className="w-9 h-9 rounded-full cursor-pointer flex items-center justify-center hover:bg-primary my-1"
         >
           <section.Icon />
-        </a>
+        </button>
       ))}
     </nav>
   );
